f79986b9ff0c1f9a928899022c113f1f04030cb8
William Sedlacek <wsedlacekc@gmail.com>
2019-10-25 09:35:56 -0500

    Fix chart page for edge

--- src/app/pages/chart.page.tsx
+++ src/app/pages/chart.page.tsx
@@ -17,8 +17,7 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.secondary.contrastText,
     backgroundColor: theme.palette.secondary.main,
     height: '100%',
-    minHeight: '100vh',
-    width: '100vw',
+    minHeight: 'calc(100vh - theme.spacing(9))',
   },
   loading: {
     position: 'absolute',
